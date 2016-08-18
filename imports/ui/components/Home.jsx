5b7043fb37982f306c3f75ab33cc896be3828efe
alonoslav <vadym.vorobel@gmail.com>
2016-08-18 07:33:39 +0300

    Add MaterialUI package

--- imports/ui/components/Home.jsx
+++ imports/ui/components/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clickCounter: 0
+    };
+  }
+
+  increaseCounter() {
+    this.setState({
+      clickCounter: this.state.clickCounter + 1
+    });
+  }
+
   render() {
     return (
-      <h3>Home</h3>
+      <div>
+        <h3>Clicked {this.state.clickCounter} times</h3>
+        <RaisedButton label="Click Me"
+                      primary={true}
+                      onClick={this.increaseCounter.bind(this)}/>
+      </div>
     );
   }
 }
--- imports/ui/layouts/MainLayout.jsx
+++ imports/ui/layouts/MainLayout.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 export default class MainLayout extends React.Component {
   render() {
     return (
-      <div className="main">
-        {this.props.content}
-      </div>
+      <MuiThemeProvider>
+        <div className="main">
+          {this.props.content}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
